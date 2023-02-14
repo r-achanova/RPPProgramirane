a59ba96c3b051856c052e5685c3fdcf5a9be7666
r-achanova <r.achanova@pmg-pernik.com>
2023-02-14 13:14:53 +0200

    add task2

---
+++ ListTasks/RemoveNonPositiveNums/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RemoveNonPositiveNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+            nums.RemoveAll(t => t < 0);
+            nums.Reverse();
+            if(nums.Count == 0)
+            {
+                Console.WriteLine("empty");
+            }
+            else
+            {
+                Console.WriteLine(string.Join(" ", nums));
+            }
+
+        }
+    }
+}
