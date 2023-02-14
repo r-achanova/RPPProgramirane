15d75a2f1031781ad24144bd275e82f57c8afe0a
r-achanova <r.achanova@pmg-pernik.com>
2023-02-14 12:58:43 +0200

    add implementation task1

--- ListTasks/RemoveNum/Program.cs
+++ ListTasks/RemoveNum/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace RemoveNum
 {
@@ -6,7 +8,12 @@ namespace RemoveNum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            List<int> nums = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            var last = nums[nums.Count - 1];
+            //nums.Remove(last);
+            nums.RemoveAll(x => x == last);
+            Console.WriteLine(string.Join(" ", nums));
+
+        } 
     }
 }
