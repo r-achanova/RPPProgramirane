1b613e20f4178e5fa491056c9bc5d48343d5f72c
r-achanova <r.achanova@pmg-pernik.com>
2023-02-14 12:34:19 +0200

    upload project

---
+++ ListTasks/RemoveNum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RemoveNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
